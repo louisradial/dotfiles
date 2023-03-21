b1dd682b0ffa3b9f73c7653864c12b3560894dfa
Louis Bergamo Radial <louis.radial@usp.br>
2023-03-21 00:05:13 -0300

    nvim: production update (pets)

---
+++ .config/nvim/snippets/all.lua
@@ -0,0 +1,6 @@
+return {
+    s({ trig = "hi" },
+        { t("Hello, world!") }),
+    s({ trig = "hii", snippetType="autosnippet"},
+        { t("Hello, world!") }),
+}
---
+++ .config/nvim/snippets/lua.lua
@@ -0,0 +1,21 @@
+
+-- return {
+--     s({ trig = ";a", snippetType = "autosnippet" },
+--         {
+--             t("\\alpha"),
+--         }),
+--
+--     -- -- snippet to make simple snippets
+--     -- s({ trig = "mkls", snippetType = "autosnippet" },
+--     --     fmta(
+--     --         [[
+--     --        s({trig="<>", snippetType="autosnippet"},
+--     --            {
+--     --                t("<>"),
+--     --            }
+--     --        ),
+--     --        ]]
+--     --     )
+--     -- ),
+--
+-- }
