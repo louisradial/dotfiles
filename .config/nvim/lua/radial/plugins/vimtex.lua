831c4954c25eba1adfeb9b6475405925dfa30969
Louis Bergamo Radial <louis.radial@usp.br>
2023-05-09 22:35:23 -0300

    nvim: add build_dir to vimtex

--- .config/nvim/lua/radial/plugins/vimtex.lua
+++ .config/nvim/lua/radial/plugins/vimtex.lua
@@ -5,6 +5,16 @@ return {
         vim.g.vimtex_view_method = 'sioyek'
         -- note: VimTex uses latexmk as default.
         -- set lualatex as default latex engine
+        vim.g.vimtex_compiler_latexmk = {
+            build_dir = '.out',
+            options = {
+                '-shell-escape',
+                '-verbose',
+                '-file-line-error',
+                '-synctex=1',
+                '-interaction=nonstopmode'
+            }
+        }
         vim.g.vimtex_compiler_latexmk_engines = { _ = '-lualatex' }
     end
 }
