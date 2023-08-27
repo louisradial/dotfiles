6571b17ddfe221aab9821e7b5245bc314f0e878e
Louis Bergamo Radial <louis.radial@usp.br>
2023-08-27 17:19:14 -0300

    wacom: intuos setup

--- .config/wacom/wacom-config.sh
+++ .config/wacom/wacom-config.sh
@@ -9,26 +9,29 @@ done
 
 list=$(xsetwacom list devices)
 
-# pad=$(echo "${list}" | awk '/pad/')
-# if [ -z "${pad}" ]; then
-#     exit 0
-# fi
-
-stylus=$(echo "${list}" | awk '/stylus/' | cut -d ' ' -f 1-7)
+stylus=$(echo "${list}" | awk '/stylus/' | cut -f 2 | cut -d ' ' -f 2)
 if [ -z "${stylus}" ]; then
     exit 0
 fi
 
-eraser=$(echo "${list}" | awk '/eraser/' | cut -d ' ' -f 1-7)
-if [ -z "${eraser}" ]; then
-    exit 0
-fi
-
-# configure the buttons on ${stylus} with your xsetwacom commands...
-#xsetwacom set "${stylus}" Button 2 11
-xsetwacom set "${stylus}" Rotate half
+# xsetwacom set "${stylus}" Rotate half
 xsetwacom set "${stylus}" MapToOutput HEAD-0
 xsetwacom set "${stylus}" Button 3 "pan"
 xsetwacom set "${stylus}" PanScrollThreshold 200
 xsetwacom set "${stylus}" Suppress 10
 xsetwacom set "${stylus}" RawSample 10
+
+pad=$(echo "${list}" | awk '/pad/' | cut -f 2 | cut -d ' ' -f 2)
+if [ -z "${pad}" ]; then
+    exit 0
+fi
+
+xsetwacom set "${pad}" Button 1 2
+xsetwacom set "${pad}" Button 2 "key ctrl c"
+xsetwacom set "${pad}" Button 3 "key ctrl v"
+xsetwacom set "${pad}" Button 8 "key ctrl z"
+
+# eraser=$(echo "${list}" | awk '/eraser/' | cut -f 2 | cut -d ' ' -f 2)
+# if [ -z "${eraser}" ]; then
+#     exit 0
+# fi
